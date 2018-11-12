dc0308e684f994ad9eda554cd68dc00618f627e2
kurtoria <victoriagronqvist@gmail.com>
2018-11-12 15:25:41 +0100

    initial commit

---
+++ Labb-2/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Labb_2
+{
+    public class Position
+    {
+        public int XPos
+        {
+            get => XPos;
+            set => XPos = value < 0 ? 0 : value;
+        }
+
+        public int YPos
+        {
+            get => YPos;
+            set => YPos = value < 0 ? 0 : value;
+        }
+
+        public Position()
+        {
+        }
+
+
+    }
+}
---
+++ Labb-2/SortedPosList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb_2
+{
+    public class SortedPosList
+    {
+        private List<Position> PositionList { get; set; }
+        public SortedPosList()
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", PositionList);
+        }
+    }
+}
